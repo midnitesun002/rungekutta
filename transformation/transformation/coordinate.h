c6d6127a639eb197f57079199121e2439423a5df
Wilbur Tinsley <jr.tinsley@gtri.gatech.edu>
2018-10-04 11:41:15 -0400

    added transformation example

---
+++ transformation/transformation/coordinate.h
@@ -0,0 +1,15 @@
+#ifndef COORDINATE_H
+#define COORDINATE_H
+
+
+class Coordinate
+{
+public:
+    Coordinate();
+private:
+    double m_x;
+    double m_y;
+    double m_z;
+};
+
+#endif // COORDINATE_H
